9369a5db3d9453f8a8e88cda919c900c75c28682
Elvio Severino <elvioseverino@gmail.com>
2023-07-13 06:12:28 -0400

    created Test component to show the structure

---
+++ resources/js/Components/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// the purpose of this file is to show how to create a component that can be imported into a Page file so it can be rendered
+function Test() {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
