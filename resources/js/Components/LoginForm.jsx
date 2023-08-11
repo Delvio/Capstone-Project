b23a7a94dc1ab22ad12ebf343d727b3c1d4ae1fc
HIUWA-CHUNG <javychunghw@gmail.com>
2023-08-11 14:48:21 -0400

    updated LoginForm

--- resources/js/Components/LoginForm.jsx
+++ resources/js/Components/LoginForm.jsx
@@ -14,72 +14,67 @@ function LoginForm() {
     }
 
     return (
-        <div className="flex flex-col justify-center content-center">
-            <form
-                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                onSubmit={submit}
-            >
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        for="email"
-                    >
-                        Email
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="email"
-                        type="text"
-                        placeholder="Email"
-                        value={data.email}
-                        onChange={(e) => setData("email", e.target.value)}
-                    />
-                    {errors.email && (
-                        <p className="text-red-500 text-xs italic">
-                            {errors.email}
-                        </p>
-                    )}
-                </div>
-                <div className="mb-6">
-                    <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        for="password"
-                    >
-                        Password
-                    </label>
-                    <input
-                        className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password"
-                        type="password"
-                        placeholder="******************"
-                        value={data.password}
-                        onChange={(e) => setData("password", e.target.value)}
-                    />
-                    {errors.password && (
-                        <p className="text-red-500 text-xs italic">
-                            {errors.password}
-                        </p>
-                    )}
-                </div>
-                <div className="flex items-center justify-between">
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                        disabled={processing}
-                    >
-                        Sign In
-                    </button>
-                    <a
-                        className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                        href="/forgot-password"
-                    >
-                        Forgot Password?
-                    </a>
-                </div>
-            </form>
-            <p className="text-center text-gray-500 text-xs">
-                &copy;2020 [name in progress]. All rights reserved.
-            </p>
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-md w-96">
+                <h2 className="text-2xl font-semibold mb-4">
+                    Log In to Your Account
+                </h2>
+                <form onSubmit={submit}>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            value={data.email}
+                            onChange={(e) => setData("email", e.target.value)}
+                            type="email"
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200"
+                            placeholder="Email"
+                        />
+                        {errors.email && (
+                            <p className="text-red-500 text-xs italic">
+                                {errors.email}
+                            </p>
+                        )}
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600">
+                            Password
+                        </label>
+                        <input
+                            id="password"
+                            placeholder="******************"
+                            value={data.password}
+                            onChange={(e) =>
+                                setData("password", e.target.value)
+                            }
+                            type="password"
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200"
+                        />
+                        {errors.password && (
+                            <p className="text-red-500 text-xs italic">
+                                {errors.password}
+                            </p>
+                        )}
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button
+                            type="submit"
+                            disabled={processing}
+                            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+                        >
+                            Log In
+                        </button>
+                        <a
+                            href="/forgot-password"
+                            className="text-blue-500 hover:underline"
+                        >
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
