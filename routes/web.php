a745a1ca6da9a49951dd8569070faa8f19b0c285
Elvio Severino <elvioseverino@gmail.com>
2023-08-11 14:48:02 -0400

    defined project routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia; // We are going to use this class to render React components
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\TaskListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,20 +22,26 @@ use App\Http\Controllers\UserController;
 //     return view('welcome');
 // });
 
+
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render("Home");
 });
 
 Route::match(["get", "post"], "/register", [UserController::class, "register"])->name("register");
-
 Route::match(["get", "post"], "/login", [UserController::class, "login"])->name("login");
 Route::match(["get", "post"], "/password-reset", [UserController::class, "passwordReset"])->name("password_reset");
-Route::post("/logout", [UserController::class, "logout"])->name("logout");
+Route::match(["get", "post"], "/logout", [UserController::class, "logout"])->name("logout");
 Route::get('/forgot-password', [UserController::class, 'showResetForm'])->name('password.request');
 Route::post('/forgot-password', [UserController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('/reset-password/{token}', [UserController::class, 'showResetPasswordForm'])->name('password.reset');
 Route::post('/reset-password', [UserController::class, 'resetPassword'])->name('password.update');
 
-Route::get("/dashboard", function () {
-    return Inertia::render("Dashboard");
-})->middleware("auth");
+Route::get('/tasklist',  [TaskListController::class, "index"])->middleware("auth");;
+Route::get('/tasklist/{id}/tasks',  [TaskListController::class, "tasks"]);
+Route::post("/tasklist/create", [TaskListController::class, "store"]);
+Route::post("/tasklist/delete", [TaskListController::class, "destroy"]);
+Route::post("/tasklist/edit", [TaskListController::class, "update"]);
+
+Route::post("/task/create", [TaskController::class, "store"]);
+Route::post("/task/edit", [TaskController::class, "update"]);
+Route::post("/task/delete", [TaskController::class, "destroy"]);
