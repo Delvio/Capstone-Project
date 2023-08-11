59b46458b33adb977b9a32f69287d44693ea3f58
HIUWA-CHUNG <javychunghw@gmail.com>
2023-08-11 14:51:06 -0400

    updated Register

--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import RegisterForm from "../Components/RegisterForm";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 
 function Register() {
     return (
-        <div>
+        <div className="flex flex-col h-screen">
+            <NavBar></NavBar>
             <RegisterForm></RegisterForm>
+            <Footer></Footer>
         </div>
     );
 }
