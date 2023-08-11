fe4864f5d4ff3d0b3cea05f75f363f558c8abf53
Elvio Severino <elvioseverino@gmail.com>
2023-08-11 14:44:07 -0400

    created dashboard page

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Tasklists from "../Components/Tasklists";
+import Footer from "../Components/Footer";
+import NavBar from "../Components/NavBar";
 
 function Dashboard() {
     return (
-        <div>
-            <form action="/logout" method="post">
-                <button type="submit">logout</button>
-            </form>
+        <div className="flex flex-col h-screen">
+            <NavBar></NavBar>
+            <Tasklists></Tasklists>
+            <Footer></Footer>
         </div>
     );
 }
